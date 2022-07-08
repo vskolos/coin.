cf135affb15561676ce2193563ab4eb3114f56fd
Vsevolod Kolos <vskolos@icloud.com>
2022-07-09 00:06:02 +0300

    change sort to ascending

--- src/blocks/accounts-list/accounts-list.js
+++ src/blocks/accounts-list/accounts-list.js
@@ -3,7 +3,6 @@ import './accounts-list.scss'
 import createAccountCard from '../account-card/account-card'
 import accounts from '../../api/accounts'
 
-// accounts = [ account1, account2, ... ]
 export default class AccountsList {
   constructor(accounts, token = localStorage.token) {
     const ul = el('ul.accounts-list')
@@ -44,19 +43,19 @@ export default class AccountsList {
     } else if (param === 'account') {
       this.accounts.sort((a, b) => a.account - b.account)
     } else if (param === 'balance') {
-      this.accounts.sort((a, b) => b.balance - a.balance)
+      this.accounts.sort((a, b) => a.balance - b.balance)
     } else if (param === 'last-transaction') {
       this.accounts.sort((a, b) => {
         if (a.transactions.length === 0 && b.transactions.length === 0) {
           return 0
         } else if (a.transactions.length === 0) {
-          return 1
-        } else if (b.transactions.length === 0) {
           return -1
+        } else if (b.transactions.length === 0) {
+          return 1
         }
         return (
-          new Date(b.transactions[b.transactions.length - 1].date).getTime() -
-          new Date(a.transactions[a.transactions.length - 1].date).getTime()
+          new Date(a.transactions[a.transactions.length - 1].date).getTime() -
+          new Date(b.transactions[b.transactions.length - 1].date).getTime()
         )
       })
     }
