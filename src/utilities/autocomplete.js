7cc6b1b44f6a8cec44cc5c38e364917faca7a050
Vsevolod Kolos <vskolos@icloud.com>
2022-07-12 14:22:39 +0300

    add timeout to blur event

--- src/utilities/autocomplete.js
+++ src/utilities/autocomplete.js
@@ -43,36 +43,33 @@ export default function autocomplete(input, array) {
     if (!list) return
     const items = list.querySelectorAll('.autocomplete-list__item')
     if (e.keyCode == 40) {
-      // If the arrow DOWN key is pressed, increase the currentFocus variable
+      // Стрелка вниз
       currentFocus++
-      // and make the current item more visible
       addActive(items)
     } else if (e.keyCode == 38) {
-      // If the arrow UP key is pressed, decrease the currentFocus variable
+      // Стрелка вверх
       currentFocus--
-      // and and make the current item more visible
       addActive(items)
     } else if (e.keyCode == 13) {
-      // If the ENTER key is pressed, prevent the form from being submitted
+      // Клавиша Enter
       e.preventDefault()
       if (currentFocus > -1) {
-        // and simulate a click on the "active" item
         if (items) items[currentFocus].click()
       }
     }
   })
 
+  input.addEventListener('blur', () => setTimeout(closeAllLists, 100))
+
   function addActive(items) {
-    // a function to classify an item as "active"
     if (!items) return false
-    // start by removing the "active" class on all items
     removeActive(items)
 
     if (currentFocus >= items.length) currentFocus = 0
     if (currentFocus < 0) currentFocus = items.length - 1
-    // add class "autocomplete-active":
     items[currentFocus].classList.add('autocomplete-list__item--active')
   }
+
   function removeActive(items) {
     // a function to remove the "active" class from all autocomplete items
     items.forEach((item) => {
@@ -89,8 +86,4 @@ export default function autocomplete(input, array) {
       }
     })
   }
-
-  document.addEventListener('click', (e) => {
-    closeAllLists(e.target)
-  })
 }
