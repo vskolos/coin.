4571222f6d35d03c27650711c2f8a31504f788bc
Vsevolod Kolos <vskolos@icloud.com>
2022-07-12 16:02:55 +0300

    add server connection error handling

--- src/pages/login.js
+++ src/pages/login.js
@@ -45,56 +45,65 @@ export default function renderLoginPage() {
     errorFieldCssClass: 'login-form__input--invalid',
   })
 
+  function closeModal() {
+    document.querySelector('.modal').remove()
+    document.body.style.removeProperty('overflow')
+  }
+
   async function sendForm() {
-    const data = await login(loginForm.login.value, loginForm.password.value)
-    if (data.error === 'No such user') {
+    try {
+      const data = await login(loginForm.login.value, loginForm.password.value)
+      if (data.error === 'No such user') {
+        const modal = createModal({
+          title: 'Ошибка',
+          text: 'Пользователя с таким логином не существует',
+          primaryButton: {
+            text: 'Закрыть',
+            clickHandler: closeModal,
+          },
+        })
+        document.body.append(modal)
+        document.body.style.overflow = 'hidden'
+        loginForm.login.value = ''
+        loginForm.password.value = ''
+      } else if (data.error === 'Invalid password') {
+        const modal = createModal({
+          title: 'Ошибка',
+          text: 'Введен неверный пароль',
+          primaryButton: {
+            text: 'Закрыть',
+            clickHandler: closeModal,
+          },
+        })
+        document.body.append(modal)
+        document.body.style.overflow = 'hidden'
+        loginForm.password.value = ''
+      } else if (!data.payload.token) {
+        const modal = createModal({
+          title: 'Ошибка',
+          text: 'Что-то пошло не так. В ответе сервера отсутствует токен. Обратитесь в техническую поддержку',
+          primaryButton: {
+            text: 'Закрыть',
+            clickHandler: closeModal,
+          },
+        })
+        document.body.append(modal)
+        document.body.style.overflow = 'hidden'
+      } else {
+        localStorage.setItem('token', data.payload.token)
+        reload()
+      }
+    } catch (err) {
       const modal = createModal({
         title: 'Ошибка',
-        text: 'Пользователя с таким логином не существует',
+        text: 'Отстутствует подключение к серверу. Обратитесь в техническую поддержку',
         primaryButton: {
           text: 'Закрыть',
-          clickHandler: () => {
-            document.querySelector('.modal').remove()
-            document.body.style.removeProperty('overflow')
-          },
+          clickHandler: closeModal,
         },
       })
       document.body.append(modal)
       document.body.style.overflow = 'hidden'
-      loginForm.login.value = ''
-      loginForm.password.value = ''
-    } else if (data.error === 'Invalid password') {
-      const modal = createModal({
-        title: 'Ошибка',
-        text: 'Введен неверный пароль',
-        primaryButton: {
-          text: 'Закрыть',
-          clickHandler: () => {
-            document.querySelector('.modal').remove()
-            document.body.style.removeProperty('overflow')
-          },
-        },
-      })
-      document.body.append(modal)
-      document.body.style.overflow = 'hidden'
-      loginForm.password.value = ''
-    } else if (!data.payload.token) {
-      const modal = createModal({
-        title: 'Ошибка',
-        text: 'Что-то пошло не так. В ответе сервера отсутствует токен. Обратитесь в техническую поддержку',
-        primaryButton: {
-          text: 'Закрыть',
-          clickHandler: () => {
-            document.querySelector('.modal').remove()
-            document.body.style.removeProperty('overflow')
-          },
-        },
-      })
-      document.body.append(modal)
-      document.body.style.overflow = 'hidden'
-    } else {
-      localStorage.setItem('token', data.payload.token)
-      reload()
     }
   }
 
