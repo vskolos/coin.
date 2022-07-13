a1afcde034d9f50c0ac96f7344f4a2fdbafc951c
Vsevolod Kolos <vskolos@icloud.com>
2022-07-13 18:12:13 +0300

    code refactor

--- src/blocks/currency-exchange-form/currency-exchange-form.js
+++ src/blocks/currency-exchange-form/currency-exchange-form.js
@@ -1,59 +1,141 @@
 import { el } from 'redom'
+import '../../../node_modules/choices.js/public/assets/styles/choices.min.css'
 import './currency-exchange-form.scss'
+import Choices from 'choices.js'
+import JustValidate from 'just-validate'
+import Modal from '../modal/modal'
 import createPrimaryButton from '../button/--primary/button--primary'
+import allCurrencies from '../../api/all-currencies'
+import currencyBuy from '../../api/currency-buy'
 import setInputFilter from '../../utilities/set-input-filter'
+import handleError from '../../utilities/handle-error'
 
-export default function createCurrencyExchangeForm(currencies) {
-  const form = el('form.currency-exchange-form')
-  const title = el('p.currency-exchange-form__title', 'Обмен валюты')
-  const options = el('.currency-exchange-form__options')
+export default class CurrencyExchangeForm {
+  constructor(parent, { accountCurrency }) {
+    this.accountCurrency = accountCurrency
+    this.element = el('form.currency-exchange-form')
 
-  const from = el(
-    'label.currency-exchange-form__label',
-    el('span.currency-exchange-form__label-text', 'Из')
-  )
-  const fromSelect = el(
-    'select.currency-exchange-form__select.js-exchange-from'
-  )
-  const to = el(
-    'label.currency-exchange-form__label',
-    el('span.currency-exchange-form__label-text', 'в')
-  )
-  const toSelect = el('select.currency-exchange-form__select.js-exchange-to')
+    const title = el('p.currency-exchange-form__title', 'Обмен валюты')
+    const options = el('.currency-exchange-form__options')
 
-  currencies.forEach((currency) => {
-    fromSelect.append(
-      el('option.currency-exchange-form__option', currency, { value: currency })
+    const from = el(
+      'label.currency-exchange-form__label',
+      el('span.currency-exchange-form__label-text', 'Из')
     )
-    toSelect.append(
-      el('option.currency-exchange-form__option', currency, { value: currency })
+    this.fromSelect = el(
+      'select.currency-exchange-form__select.js-exchange-from'
     )
-  })
+    const to = el(
+      'label.currency-exchange-form__label',
+      el('span.currency-exchange-form__label-text', 'в')
+    )
+    this.toSelect = el('select.currency-exchange-form__select.js-exchange-to')
 
-  const amount = el('label.currency-exchange-form__label', 'Сумма')
-  const amountInput = el(
-    'input.currency-exchange-form__input.currency-exchange-form__input--amount',
-    {
-      placeholder: '0.1235421',
-      name: 'amount',
-    }
-  )
-  amount.append(amountInput)
+    const amount = el('label.currency-exchange-form__label', 'Сумма')
+    const amountInput = el(
+      'input.currency-exchange-form__input.currency-exchange-form__input--amount',
+      {
+        placeholder: '0.1235421',
+        name: 'amount',
+      }
+    )
+    amount.append(amountInput)
 
-  setInputFilter(amountInput, (value) => /^\d*\.?\d*$/.test(value))
+    setInputFilter(amountInput, (value) => /^\d*\.?\d*$/.test(value))
 
-  const button = createPrimaryButton({ text: 'Обменять' })
-  button.type = 'submit'
-  button.name = 'submit'
+    const button = createPrimaryButton({ text: 'Обменять' })
+    button.type = 'submit'
+    button.name = 'submit'
 
-  from.append(fromSelect)
-  to.append(toSelect)
-  options.append(from, to, amount)
-  form.append(title, options, button)
+    from.append(this.fromSelect)
+    to.append(this.toSelect)
+    options.append(from, to, amount)
+    this.element.append(title, options, button)
+    parent.append(this.element)
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-  })
+    this.element.addEventListener('submit', (e) => {
+      e.preventDefault()
+    })
 
-  return form
+    allCurrencies()
+      .then((response) => {
+        if (response.error) {
+          throw new Error(response.error)
+        }
+        return response.payload
+      })
+      .then((currencies) => {
+        currencies.forEach((currency) => {
+          this.fromSelect.append(
+            el('option.currency-exchange-form__option', currency, {
+              value: currency,
+            })
+          )
+          this.toSelect.append(
+            el('option.currency-exchange-form__option', currency, {
+              value: currency,
+            })
+          )
+        })
+        this.accountCurrency.rows = currencies.length
+        this.init()
+      })
+      .catch((error) => handleError(error))
+  }
+
+  init(fromChoice = 'BTC', toChoice = 'ETH') {
+    const fromChoices = new Choices(this.fromSelect, {
+      allowHTML: false,
+      searchEnabled: false,
+      itemSelectText: '',
+    })
+    fromChoices.setChoiceByValue(fromChoice)
+
+    const toChoices = new Choices(this.toSelect, {
+      allowHTML: false,
+      searchEnabled: false,
+      itemSelectText: '',
+    })
+    toChoices.setChoiceByValue(toChoice)
+
+    const validation = new JustValidate(this.element, {
+      errorLabelStyle: {},
+      errorLabelCssClass: 'currency-exchange-form__label-text--invalid',
+      errorFieldCssClass: 'currency-exchange-form__input--invalid',
+    })
+
+    validation
+      .addField('.currency-exchange-form__input--amount', [
+        {
+          rule: 'required',
+          errorMessage: 'Введите сумму перевода',
+        },
+      ])
+      .onSuccess(async () => {
+        try {
+          const response = await currencyBuy(
+            {
+              from: fromChoices.getValue().value,
+              to: toChoices.getValue().value,
+              amount: this.element.amount.value,
+            },
+            localStorage.token
+          )
+          if (response.error) {
+            throw new Error(response.error)
+          }
+          const modal = new Modal({
+            title: 'Обмен завершён',
+            text: `Вы обменяли ${this.element.amount.value} ${
+              fromChoices.getValue().value
+            } на ${toChoices.getValue().value}`,
+          })
+          modal.open()
+          this.accountCurrency.fetch()
+          this.element.amount.value = ''
+        } catch (error) {
+          handleError(error)
+        }
+      })
+  }
 }
--- src/pages/currency.js
+++ src/pages/currency.js
@@ -1,11 +1,6 @@
 // CSS
 import 'normalize.css'
 import '../common/common.scss'
-import '../../node_modules/choices.js/public/assets/styles/choices.min.css'
-
-// Libraries
-import Choices from 'choices.js'
-import JustValidate from 'just-validate'
 
 // Blocks
 import createHeader from '../blocks/header/header'
@@ -14,23 +9,14 @@ import createContainer from '../blocks/container/container'
 import createTopRow from '../blocks/top-row/top-row'
 import createCurrencyInfo from '../blocks/currency-info/currency-info'
 import AccountCurrency from '../blocks/account-currency/account-currency'
-import createCurrencyExchangeForm from '../blocks/currency-exchange-form/currency-exchange-form'
+import CurrencyExchangeForm from '../blocks/currency-exchange-form/currency-exchange-form'
 import CurrencyFeed from '../blocks/currency-feed/currency-feed'
-import Modal from '../blocks/modal/modal'
-
-// API
-import allCurrencies from '../api/all-currencies'
-import currencyBuy from '../api/currency-buy'
 
 // Utilities
 import logout from '../utilities/logout'
 import reload from '../app'
-import handleError from '../utilities/handle-error'
 
-export default async function renderCurrencyPage() {
-  const response = await allCurrencies()
-  const allCurrenciesList = response.payload
-
+export default function renderCurrencyPage() {
   const body = document.body
   const header = createHeader([
     { text: 'Банкоматы', disabled: false, handler: () => reload('/banks') },
@@ -48,82 +34,22 @@ export default async function renderCurrencyPage() {
     title: 'Валютный обмен',
   })
   const currencyInfo = createCurrencyInfo()
+
   const accountCurrency = new AccountCurrency()
-  const currencyExchangeForm = createCurrencyExchangeForm(allCurrenciesList)
+  currencyInfo.append(accountCurrency.element)
+
+  new CurrencyExchangeForm(currencyInfo, { accountCurrency: accountCurrency })
 
   const currencyFeed = new CurrencyFeed()
   accountCurrency.rows.then((rows) => {
-    console.log(accountCurrency.rows)
     currencyFeed.rows = rows + 6
   })
 
-  currencyInfo.append(
-    accountCurrency.element,
-    currencyExchangeForm,
-    currencyFeed.element
-  )
+  currencyInfo.append(currencyFeed.element)
   mainContainer.append(topRow, currencyInfo)
 
   main.append(mainContainer)
 
   body.innerHTML = ''
   body.append(header, main)
-
-  const fromSelect = currencyExchangeForm.querySelector('.js-exchange-from')
-  const fromChoices = new Choices(fromSelect, {
-    allowHTML: false,
-    searchEnabled: false,
-    itemSelectText: '',
-  })
-  fromChoices.setChoiceByValue('BTC')
-
-  const toSelect = currencyExchangeForm.querySelector('.js-exchange-to')
-  const toChoices = new Choices(toSelect, {
-    allowHTML: false,
-    searchEnabled: false,
-    itemSelectText: '',
-  })
-  toChoices.setChoiceByValue('ETH')
-
-  const validation = new JustValidate(currencyExchangeForm, {
-    errorLabelStyle: {},
-    errorLabelCssClass: 'currency-exchange-form__label-text--invalid',
-    errorFieldCssClass: 'currency-exchange-form__input--invalid',
-  })
-
-  async function sendForm() {
-    try {
-      const response = await currencyBuy(
-        {
-          from: fromChoices.getValue().value,
-          to: toChoices.getValue().value,
-          amount: currencyExchangeForm.amount.value,
-        },
-        localStorage.token
-      )
-      if (response.error) {
-        throw new Error(response.error)
-      }
-      const modal = new Modal({
-        title: 'Обмен завершён',
-        text: `Вы обменяли ${currencyExchangeForm.amount.value} ${
-          fromChoices.getValue().value
-        } на ${toChoices.getValue().value}`,
-      })
-      modal.open()
-      accountCurrency.fetch()
-      currencyExchangeForm.amount.value = ''
-    } catch (error) {
-      handleError(error)
-    }
-  }
-
-  validation
-    .addField('.currency-exchange-form__input--amount', [
-      {
-        rule: 'required',
-        errorMessage: 'Введите сумму перевода',
-      },
-    ])
-    .onSuccess(sendForm)
 }
